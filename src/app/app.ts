9447e9322de324c7f8dce6d3f95015bf33b00829
VinhTin-AQUA <tinhovinh@gmail.com>
2025-08-07 22:08:11 +0700

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('FirstApp');
+  protected readonly title = signal('Hello world');
 }
